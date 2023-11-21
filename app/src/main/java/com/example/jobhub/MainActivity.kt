277b01f4e16f586e14cf99b68b6c77ee9ca232c6
Maria Eduarda <135435746+ggggKKKKK@users.noreply.github.com>
2023-11-21 17:29:21 -0300

    +1

--- app/src/main/java/com/example/jobhub/MainActivity.kt
+++ app/src/main/java/com/example/jobhub/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.jobhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -32,7 +37,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,20 +47,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JobHubTheme {
-                Tela()
+                JobHub()
             }
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showSystemUi = true, showBackground = true,backgroundColor = 55)
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 55)
 @Composable
-fun Tela(){
-    var User by remember{ mutableStateOf("") }
-    var IdTexto = 1
-    var Log = User
-    var Password by remember{ mutableStateOf("") }
-    var Pass = Password
+fun JobHub() {
+    var name by remember { mutableStateOf("") }
+    var UserOrEmail by remember { mutableStateOf("") }
+    var Password by remember { mutableStateOf("") }
+    var ConfirmPassword by remember { mutableStateOf("") }
+    var Register by remember { mutableStateOf(false) }
     Image(
         painter = painterResource(id = R.drawable.azul),
         contentDescription = null,
@@ -67,11 +70,11 @@ fun Tela(){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(top = 200.dp).fillMaxSize()
+            .fillMaxSize()
+            .padding(top = 150.dp)
     ) {
-        IdTexto = R.string.Main
         Text(
-            text = stringResource(IdTexto),
+            text = stringResource(id = R.string.Main),
             color = Color.White,
             fontSize = 50.sp,
             fontWeight = FontWeight.ExtraBold,
@@ -83,57 +86,112 @@ fun Tela(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(12.dp),
         modifier = Modifier
-            .padding(top = 450.dp).fillMaxSize()
-    ) {
-        TextField(
-            value = User,
-            onValueChange = { User = it },
-            label = {
-                Text(
-                    text = "Email or User"
+            .padding(top = 350.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessHigh
                 )
-            },
-            isError = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text
             )
-        )
-        TextField(
+    ) {
+        if (Register){
+
+            CamposDeTexto(
+                value = name,
+                onValueChange = {name=it},
+                idTexto = R.string.name
+            )
+        }
+        CamposDeTexto(
             value = Password,
             onValueChange = { Password = it },
-            label = {
-                Text(
-                    text = "Password"
-                )
-            },
-            isError = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text
-            )
+            idTexto = R.string.Login,
         )
-        Row (
-            horizontalArrangement = Arrangement.spacedBy(15.dp),
-            verticalAlignment = Alignment.CenterVertically
 
-        ){
-            Contas(id = R.drawable.facebook)
-            Contas(id = R.drawable.google)
-            Contas(id = R.drawable.linkedin)
+        CamposDeTexto(
+            value = UserOrEmail,
+            onValueChange = { UserOrEmail = it },
+            idTexto = R.string.Pass
+        )
+        if (Register){
+
+            CamposDeTexto(value = ConfirmPassword,
+                onValueChange = {ConfirmPassword=it},
+                idTexto = R.string.Confirm)
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Button(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "Sing in"
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(25.dp),
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                FormasDeLogin(id = R.drawable.facebook)
+                FormasDeLogin(id = R.drawable.google)
+                FormasDeLogin(id = R.drawable.linkedin)
+            }
+            Spacer(modifier = Modifier.size(10.dp))
+            if (!Register) {
+                Text(
+                    text = if(Register){
+                    "Sing in"
+                    }else
+                        "Register now ",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Black,
+                    modifier = Modifier
+                        .clickable {
+                            Register = true
+                        }
+                )
+            }
         }
     }
 }
+
 @Composable
-fun Contas(id:Int){
+fun FormasDeLogin(id: Int) {
     val OnClickImage = {}
 
     Image(
         painter = painterResource(id = id),
         contentDescription = null,
         modifier = Modifier
-            .clickable (onClick = OnClickImage)
+            .clickable(onClick = OnClickImage)
             .size(35.dp)
             .clip(CircleShape)
-            .border(1.5.dp, Color(2,16,237), CircleShape)
+            .border(1.5.dp, Color(2, 16, 237), CircleShape)
 
     )
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CamposDeTexto(
+    value: String,
+    onValueChange: (String) -> Unit,
+    idTexto: Int
+
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(
+                text = stringResource(id = idTexto)
+            )
+        }
+    )
+}
+
